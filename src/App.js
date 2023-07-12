f02b3a31d621404e60333456627e06af46af7d75
Daniel Janson <70939410+DanielAugustJanson@users.noreply.github.com>
2023-07-12 07:25:14 +0300

    Setup Complete
    Assets added, Pages created, SPA decided upon.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
+import { useState } from 'react';
 import './App.css';
+import Navbar from './comp/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+import Order from './pages/Order';
+import Footer from './comp/Footer';
 
 function App() {
+  const[language,setLanguage] = useState("EST")
+  const [activeLink, setActiveLink] = useState('Home');
+  
+  const apiKey = process.env.REACT_APP_GALLERY_KEY;
+
+  //Render website
+  function render(){
+    switch(activeLink){
+      case "Home":
+        return <Home></Home>
+        break;
+      case "About":
+        return <About></About>
+        break;
+      case "Gallery":
+        return <Gallery></Gallery>
+        break;
+      case "Contact":
+        return <Contact></Contact>
+        break;
+      case "Order":
+        return <Order></Order>
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar language={language} activeLink={activeLink} setActiveLink={setActiveLink}></Navbar>
+      {render()}
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/pages/Order.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Order = props => {
+  return (
+    <div>Order</div>
+  )
+}
+
+Order.propTypes = {}
+
+export default Order
