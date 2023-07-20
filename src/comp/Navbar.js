e982912e78562d6954534fc1cd78459b611b58d5
Daniel Janson <70939410+DanielAugustJanson@users.noreply.github.com>
2023-07-21 01:36:25 +0300

    Logo added and reroute complete

--- src/comp/Navbar.js
+++ src/comp/Navbar.js
@@ -1,48 +1,73 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
-import "./Navbar.css"
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./Navbar.css";
+import GraoBorder from "./../assets/logos/grao.13x8mm.png";
+import GraoGold from "./../assets/logos/grao.30x47.png";
+import GraoBlack from "./../assets/logos/grao.30x47B2.png";
+import GraoWhite from "./../assets/logos/grao.30x472.png";
 
-const Navbar = ({language,activeLink,setActiveLink}) => {
-  
+const Navbar = ({ language, activeLink, setActiveLink }) => {
   return (
-    <div className='navDiv'>
-      <div className='logos'>
-        
+    <nav>
+      <div className="navDiv">
+        <div className="logos">
+          <img alt="Grao Logo" src={GraoBlack} onClick={()=>{
+            setActiveLink("Home")
+          }}></img>
+        </div>
+
+        <div className="links">
+          <p
+            onClick={() => {
+              setActiveLink("Home");
+            }}
+            id="Home"
+          >
+            Home
+          </p>
+
+          <p
+            onClick={() => {
+              setActiveLink("About");
+            }}
+            id="About"
+          >
+            About
+          </p>
+
+          <p
+            onClick={() => {
+              setActiveLink("Gallery");
+            }}
+            id="Gallery"
+          >
+            Gallery
+          </p>
+
+          <p
+            onClick={() => {
+              setActiveLink("Contact");
+            }}
+            id="Contact"
+          >
+            Contact
+          </p>
+
+          <p
+            onClick={() => {
+              setActiveLink("Order");
+            }}
+            id="Order"
+          >
+            Order
+          </p>
+        </div>
       </div>
-      
-      <div className='links'>
+    </nav>
+  );
+};
 
-        <p onClick={()=>{
-          setActiveLink("Home")
-        }}
-        id='Home'>Home</p>
-        
-        <p onClick={()=>{
-          setActiveLink("About")
-        }}
-        id='About'>About</p>
+Navbar.propTypes = {};
 
-        <p onClick={()=>{
-          setActiveLink("Gallery")
-        }}
-        id='Gallery'>Gallery</p>
-
-        <p onClick={()=>{
-          setActiveLink("Contact")
-        }}
-        id='Contact'>Contact</p>
-
-        <p onClick={()=>{
-          setActiveLink("Order")
-        }}
-        id='Order'>Order</p>
-
-      </div>
-    </div>
-  )
-}
-
-Navbar.propTypes = {}
-
-export default Navbar
+export default Navbar;
