df9ecd10c1d1d41b401de28b454b949d4b50ef14
Daniel Janson <70939410+DanielAugustJanson@users.noreply.github.com>
2023-07-22 19:57:10 +0300

    Map working and Styling done for now

--- src/comp/widgets/Maps.js
+++ src/comp/widgets/Maps.js
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import PropTypes from "prop-types";
-import GoogleMapReact from "google-map-react";
+import { GoogleMapsProvider } from "@ubilabs/google-maps-react-hooks";
 
-const Maps = ({data}) => {
-
-    const defaultProps = {
-        center: {
-          lat: 59.43274889412256,
-          lng: 24.74463366254776
-        },
-        zoom: 16,
-      };
+/* Currently not in use. */
 
 
-  return;
+const Maps = ({ data }) => {
+  const [mapContainer, setMapContainer] = useState(null);
+  const ref = useCallback(node => {
+    node && setMapContainer(node);
+  }, []);
 
-};
+  const mapOptions = {
+    // Add your map options here
+    // `center` and `zoom` are required for every map to be displayed
+    center: {lat: 53.5582447, lng: 9.647645},
+    zoom: 16
+  };
+
+  return (
+    <GoogleMapsProvider
+      googleMapsAPIKey={process.env.REACT_APP_MAPS_API_KEY}
+      mapContainer={mapContainer}
+      mapOptions={mapOptions}>
+      <React.StrictMode>
+        <div ref={ref} style={{height: '100%'}} />
+      </React.StrictMode>
+    </GoogleMapsProvider>
+  );
+}
 
 Maps.propTypes = {};
 
 export default Maps;
+
