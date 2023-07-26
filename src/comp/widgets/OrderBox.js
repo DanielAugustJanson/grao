bb389f404eac4ce47a9d359ecc444a5f0a34ca59
Daniel Janson <70939410+DanielAugustJanson@users.noreply.github.com>
2023-07-27 01:17:03 +0300

    Order Box made
    Integration of email_JS in progress

--- src/comp/widgets/OrderBox.js
+++ src/comp/widgets/OrderBox.js
@@ -1,19 +1,51 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import "./OrderBox.css"
-import { TextField } from '@mui/material'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./OrderBox.css";
+import { Button, TextField } from "@mui/material";
 
-const OrderBox = props => {
+const OrderBox = (props) => {
+  const[emailStatus, setEmailStatus] = useState(false)
+  const[emailMsg, setEmailMsg] = useState(null)
+
+
   return (
-    <div id='OrderBox'>
-        <TextField id="NameInput" label="Name" variant="outlined" />
-        <TextField id="EmailInput" label="Email" variant="outlined" />
-        <TextField id="PhoneIntput" label="Phone" variant="outlined" />
-        <TextField id="MessageInput" label="Message" variant="outlined" />
+    <div id="OrderBox">
+      <div className="orderSpanner">
+        <TextField
+          sx={{ width: "300px" }}
+          id="NameInput"
+          label="Name"
+          variant="outlined"
+        />
+        <TextField
+          sx={{ width: "300px" }}
+          id="PhoneIntput"
+          label="Phone"
+          variant="outlined"
+        />
+      </div>
+      <TextField
+        sx={{ width: "610px" }}
+        id="EmailInput"
+        label="Email"
+        variant="outlined"
+      />
+      <TextField
+        sx={{ width: "610px" }}
+        multiline
+        id="MessageInput"
+        rows={5}
+        label="Message"
+        variant="outlined"
+      />
+      <Button sx={{ width: "100px" }} variant="outlined" color="success" onClick={()=>{setEmailStatus(true)}}>
+        Submit
+      </Button>
+      {emailStatus && (<p>Your order has been submitted. We will contact you at first chance.</p>)}
     </div>
-  )
-}
+  );
+};
 
-OrderBox.propTypes = {}
+OrderBox.propTypes = {};
 
-export default OrderBox
+export default OrderBox;
