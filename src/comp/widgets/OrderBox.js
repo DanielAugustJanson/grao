53d6eaa5b47e8c83e67db640812a879cc52481ef
Daniel Janson <70939410+DanielAugustJanson@users.noreply.github.com>
2023-07-27 11:21:14 +0300

    Order Box WIP

--- src/comp/widgets/OrderBox.js
+++ src/comp/widgets/OrderBox.js
@@ -5,23 +5,70 @@ import { Button, TextField } from "@mui/material";
 
 const OrderBox = (props) => {
   const[emailStatus, setEmailStatus] = useState(false)
-  const[emailMsg, setEmailMsg] = useState(null)
+
+  const[nameEmpty,setNameEmpty] = useState(false)
+  const[emailEmpty,setEmailEmpty] = useState(false)
+  const[messageEmpty,setMessageEmpty] = useState(false)
+
+  //Form Details
+  const[formName,setFormName] = useState("")
+  const[formNumber,setFormNumber] = useState("")
+  const[formEmail,setFormEmail] = useState("")
+  const[formMessage,setFormMessage] = useState("")
+
+  function checkForm(){
+      let result = true;
+
+      if(formName === ""){
+        result = false;
+        setNameEmpty(true);
+      }
+      else{
+        setNameEmpty(false)
+      }
+      ///////////////////////
+      if(formEmail === ""){
+        result = false;
+        setEmailEmpty(true)
+      }else{
+        setEmailEmpty(false)
+      }
+      ///////////////////////
+      if(formMessage === ""){
+        result = false;
+        setMessageEmpty(true)
+      }else{
+        
+        setMessageEmpty(false)
+      }
+
+      return result;
+  }
+
+  function sendForm(){
+    checkForm()
+  }
 
 
   return (
     <div id="OrderBox">
+      <h2>Sumbit order</h2>
       <div className="orderSpanner">
         <TextField
           sx={{ width: "300px" }}
           id="NameInput"
           label="Name"
           variant="outlined"
+          required
+          error={nameEmpty}
+          onChange={(e)=>{setFormName(e.target.value)}}
         />
         <TextField
           sx={{ width: "300px" }}
           id="PhoneIntput"
           label="Phone"
           variant="outlined"
+          onChange={(e)=>{setFormNumber(e.target.value)}}
         />
       </div>
       <TextField
@@ -29,16 +76,22 @@ const OrderBox = (props) => {
         id="EmailInput"
         label="Email"
         variant="outlined"
+        required
+        error={emailEmpty}
+        onChange={(e)=>{setFormEmail(e.target.value)}}
       />
       <TextField
-        sx={{ width: "610px" }}
+        sx={{ width: "610px"}}
         multiline
         id="MessageInput"
         rows={5}
         label="Message"
         variant="outlined"
+        required
+        error={messageEmpty}
+        onChange={(e)=>{setFormMessage(e.target.value)}}
       />
-      <Button sx={{ width: "100px" }} variant="outlined" color="success" onClick={()=>{setEmailStatus(true)}}>
+      <Button sx={{ width: "100px" }} variant="outlined" color="success" onClick={()=>{sendForm()}}>
         Submit
       </Button>
       {emailStatus && (<p>Your order has been submitted. We will contact you at first chance.</p>)}
@@ -49,3 +102,6 @@ const OrderBox = (props) => {
 OrderBox.propTypes = {};
 
 export default OrderBox;
+
+
+/* rebakat357@inkiny.com */
--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import "./Order.css"
 import OrderBox from '../comp/widgets/OrderBox'
+import OderBG from "./../assets/images/C1.jpg"
 
 const Order = props => {
   return (
     <div>
       <section id='OrderLanding'>
-        <h2>Sumbit order</h2>
+        <img alt='Pen and Paper' src={OderBG}></img>
         <OrderBox/>
       </section>
     </div>
