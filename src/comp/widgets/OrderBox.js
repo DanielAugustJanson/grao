ac920c0ad5a571041640bb632aa2dce612e3736e
Daniel Janson <70939410+DanielAugustJanson@users.noreply.github.com>
2023-07-28 03:19:51 +0300

    Redoing the Oder Box. Issues

--- src/comp/widgets/OrderBox.js
+++ src/comp/widgets/OrderBox.js
@@ -1,101 +1,97 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import PropTypes from "prop-types";
 import "./OrderBox.css";
 import { Button, TextField } from "@mui/material";
+import emailjs from "@emailjs/browser";
 
 const OrderBox = (props) => {
-  const[emailStatus, setEmailStatus] = useState(false)
+  ///////////////////////////////////////////////////////////////////////
+  const form = useRef();
+  const [emailStatus, setEmailStatus] = useState(false);
 
-  const[nameEmpty,setNameEmpty] = useState(false)
-  const[emailEmpty,setEmailEmpty] = useState(false)
-  const[messageEmpty,setMessageEmpty] = useState(false)
+
+  const [nameEmpty, setNameEmpty] = useState(false);
+  const [emailEmpty, setEmailEmpty] = useState(false);
+  const [messageEmpty, setMessageEmpty] = useState(false);
 
   //Form Details
-  const[formName,setFormName] = useState("")
-  const[formNumber,setFormNumber] = useState("")
-  const[formEmail,setFormEmail] = useState("")
-  const[formMessage,setFormMessage] = useState("")
+  const [formName, setFormName] = useState("");
+  const [formNumber, setFormNumber] = useState("");
+  const [formEmail, setFormEmail] = useState("");
+  const [formMessage, setFormMessage] = useState("");
 
-  function checkForm(){
-      let result = true;
+  ////////////////////////////////////////////////////////////////////////
 
-      if(formName === ""){
-        result = false;
-        setNameEmpty(true);
-      }
-      else{
-        setNameEmpty(false)
-      }
-      ///////////////////////
-      if(formEmail === ""){
-        result = false;
-        setEmailEmpty(true)
-      }else{
-        setEmailEmpty(false)
-      }
-      ///////////////////////
-      if(formMessage === ""){
-        result = false;
-        setMessageEmpty(true)
-      }else{
-        
-        setMessageEmpty(false)
-      }
+  let tempalteParams = {
+    formName: formName,
+    formNumber: formNumber,
+    formEmail: formEmail,
+    formMessage: formMessage,
+  };
 
-      return result;
+  function checkForm() {
+    let result = true;
+
+    if (formName === "") {
+      result = false;
+      setNameEmpty(true);
+    } else {
+      setNameEmpty(false);
+    }
+    ///////////////////////
+    if (formEmail === "") {
+      result = false;
+      setEmailEmpty(true);
+    } else {
+      setEmailEmpty(false);
+    }
+    ///////////////////////
+    if (formMessage === "") {
+      result = false;
+      setMessageEmpty(true);
+    } else {
+      setMessageEmpty(false);
+    }
+
+    return result;
   }
 
-  function sendForm(){
-    checkForm()
+  function sendForm() {
+    sendEmail()
   }
 
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_44ntx5g",
+        "template_cqds9hq",
+        form.current,
+        "uMdPW65UGfGCWdsTe"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          window.alert("Sent");
+        },
+        (error) => {
+          console.log(error.text);
+          window.alert("An error occured. Try Again");
+        }
+      );
+  }
 
   return (
-    <div id="OrderBox">
-      <h2>Sumbit order</h2>
-      <div className="orderSpanner">
-        <TextField
-          sx={{ width: "300px" }}
-          id="NameInput"
-          label="Name"
-          variant="outlined"
-          required
-          error={nameEmpty}
-          onChange={(e)=>{setFormName(e.target.value)}}
-        />
-        <TextField
-          sx={{ width: "300px" }}
-          id="PhoneIntput"
-          label="Phone"
-          variant="outlined"
-          onChange={(e)=>{setFormNumber(e.target.value)}}
-        />
-      </div>
-      <TextField
-        sx={{ width: "610px" }}
-        id="EmailInput"
-        label="Email"
-        variant="outlined"
-        required
-        error={emailEmpty}
-        onChange={(e)=>{setFormEmail(e.target.value)}}
-      />
-      <TextField
-        sx={{ width: "610px"}}
-        multiline
-        id="MessageInput"
-        rows={5}
-        label="Message"
-        variant="outlined"
-        required
-        error={messageEmpty}
-        onChange={(e)=>{setFormMessage(e.target.value)}}
-      />
-      <Button sx={{ width: "100px" }} variant="outlined" color="success" onClick={()=>{sendForm()}}>
-        Submit
-      </Button>
-      {emailStatus && (<p>Your order has been submitted. We will contact you at first chance.</p>)}
-    </div>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
   );
 };
 
@@ -103,5 +99,41 @@ OrderBox.propTypes = {};
 
 export default OrderBox;
 
-
 /* rebakat357@inkiny.com */
+
+
+/*
+
+
+
+<>
+      <div id="OrderBox">
+        <form ref={form} onSubmit={sendEmail}>
+          <div className="horSpanner">
+            <div className="vertSpanner">
+              <label>Name</label>
+              <input type="text" id="NameInput" name="user_name" />
+            </div>
+            <div className="vertSpanner">
+              <label>Email</label>
+              <input type="email" id="EmailInput" name="user_email" />
+            </div>
+          </div>
+          <div className="vertSpanner">
+            <label>Message</label>
+            <textarea id="MessageInput" name="message" />
+          </div>
+          <input id="SubmitInput" type="submit" value="Send" />
+        </form>
+      </div>
+      {emailStatus && (
+        <p>
+          Your order has been submitted. We will contact you at first chance.
+        </p>
+      )}
+    </>
+
+
+
+
+*/
