d4f38d8f1c9c81a5c6bb59fbcd857163044566b5
Daniel Janson <70939410+DanielAugustJanson@users.noreply.github.com>
2023-08-04 00:26:41 +0300

    S

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./About.css";
+import aboutJson from "./../content/about.json"
 
 // This image is to be changed
 import AboutLanding from "./../assets/gallery/B3.jpeg";
@@ -9,35 +10,37 @@ import AboutI from "./../assets/gallery/B6.jpg";
 import AboutII from "./../assets/gallery/B5.jpg";
 import AboutIII from "./../assets/gallery/B2.jpg";
 
-const About = (props) => {
+const About = ({language}) => {
   return (
     <div id="AboutPage">
       <section id="AboutLanding">
         <img alt="Grao title with flowers" src={AboutLanding}></img>
-        <h2>About Us</h2>
+        <h2>{aboutJson[language]["titleLanding"]}</h2>
       </section>
+      <br/>
       <section id="AboutSection-I">
         <div className="imgDiv">
           <img alt="food plates" src={AboutI}></img>
         </div>
         <div className="textDiv">
-          <h2>Kohvik Grao</h2>
+          <h2>{aboutJson[language]["AboutSectionI"]["h2"]}</h2>
           <p>
-            {}
+            {aboutJson[language]["AboutSectionI"]["p1"]}
           </p>
           <p>
-            {}
+            {aboutJson[language]["AboutSectionI"]["p2"]}
           </p>
         </div>
       </section>
+      <br/>
       <section id="AboutSection-II">
         <div className="textDiv">
-          <h2>Kohvik Grao</h2>
+        <h2>{aboutJson[language]["AboutSectionII"]["h2"]}</h2>
           <p>
-            {}
+            {aboutJson[language]["AboutSectionII"]["p1"]}
           </p>
           <p>
-            {}
+            {aboutJson[language]["AboutSectionII"]["p2"]}
           </p>
         </div>
         <div className="imgDiv">
