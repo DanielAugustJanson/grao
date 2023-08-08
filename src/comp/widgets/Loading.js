31c2b193b23f38f614f382520995656844896967
Daniel Janson <70939410+DanielAugustJanson@users.noreply.github.com>
2023-08-08 12:17:00 +0300

    Loading added, but brokish

---
+++ src/comp/widgets/Loading.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import "./Loading.css"
+import loadingGif from "./../../assets/icons/Eclipse-2s.svg"
+
+const Loading = ({loading}) => {
+  const[fadeOut, setFadeOut] = useState("");
+
+  useEffect(() => {
+    if (!loading) {
+      // Delay the fade-out effect to let the loading image show for a moment
+      setTimeout(() => {
+        setFadeOut("fade-out");
+      }, 400);
+      console.log("Use Effect used")
+    }
+  }, [loading]);
+
+
+  return (
+    <div id='loadingDiv' className={fadeOut}>
+        <img alt='loading' src={loadingGif} ></img>
+    </div>
+  )
+}
+
+Loading.propTypes = {}
+export default Loading
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import "./Home.css";
 import GraoBlack from "./../assets/logos/grao.30x47B2.png";
@@ -6,39 +6,53 @@ import LandingImg from "./../assets/grao/G1.JPG";
 import G1 from "./../assets/grao/G6.png";
 import G2 from "./../assets/grao/G7.png";
 import G3 from "./../assets/grao/G8.png";
-import homeJson from "./../content/home.json"
+import homeJson from "./../content/home.json";
 
 import Menu from "./../assets/menu/menu.jpg";
+import Loading from "./../comp/widgets/Loading";
 
 const Home = ({ language, setActiveLink }) => {
+  const [loading, setLoading] = useState(true);
+  const [show, setShow] = useState(true);
+
   return (
     <div id="HomePage">
+      {show ? <Loading loading={loading}/> : null}
       <section id="HomeLanding">
-        <img alt="Cafe" src={LandingImg}></img>
+        <img
+          alt="Cafe"
+          src={LandingImg}
+          onLoad={() => {
+            setLoading(false);
+            setTimeout(() => {
+              setShow(false)       
+            }, 500);
+          }}
+        ></img>
         <div className="landingText">
           <h1>{homeJson[language]["titleLanding"]}</h1>
         </div>
       </section>
       {/*--------------------------------------------------------------------------------*/}
       <section id="HomeContent">
-        <div className="HomeImgI"><img src={G1} alt="cafe"></img></div>
-        <div className="HomeImgII"><img src={G2} alt="cafe"></img></div>
-        <div className="HomeImgIII"><img src={G3} alt="cafe"></img></div>
-        
-        
-        
+        <div className="HomeImgI">
+          <img src={G1} alt="cafe"></img>
+        </div>
+        <div className="HomeImgII">
+          <img src={G2} alt="cafe"></img>
+        </div>
+        <div className="HomeImgIII">
+          <img src={G3} alt="cafe"></img>
+        </div>
+
         <div className="HomeDivText">
           <h2>{homeJson[language]["HomeSecI"]["h2"]}</h2>
-          <p>
-          {homeJson[language]["HomeSecI"]["p1"]}
-          </p>
-          <p>
-          {homeJson[language]["HomeSecI"]["p2"]}
-          </p>
+          <p>{homeJson[language]["HomeSecI"]["p1"]}</p>
+          <p>{homeJson[language]["HomeSecI"]["p2"]}</p>
           <h3
             onClick={() => {
               setActiveLink("About");
-              window.scrollTo(0, 0)
+              window.scrollTo(0, 0);
             }}
           >
             {homeJson[language]["HomeSecI"]["h3"]}
@@ -48,9 +62,9 @@ const Home = ({ language, setActiveLink }) => {
 
       {/*--------------------------------------------------------------------------------*/}
       <section id="HomeMenu">
-      <img id="menuLogo" alt="grao logo" src={GraoBlack}></img>
-          <h2>{homeJson[language]["menu"]}</h2>
-          <img id="menuImg" alt="menu" src={Menu}></img>
+        <img id="menuLogo" alt="grao logo" src={GraoBlack}></img>
+        <h2>{homeJson[language]["menu"]}</h2>
+        <img id="menuImg" alt="menu" src={Menu}></img>
       </section>
     </div>
   );
